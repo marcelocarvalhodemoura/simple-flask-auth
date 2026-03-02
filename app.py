7f1e4875703e3211ac5c511e417ef242edfbb100
marcelocarvalhodemoura <marcelo@devwev.com.br>
2026-03-02 15:50:33 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,117 @@
+import os
+from flask import Flask, request, jsonify
+from models.user import User
+from database import db
+from flask_login import LoginManager, login_user, login_required, logout_user
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key'
+app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{os.path.join(basedir, "instance", "database.db")}'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+login_manager = LoginManager(app)
+db.init_app(app) # conecta ao banco de dados
+login_manager.init_app(app)
+
+# view login
+login_manager.login_view = 'login'
+# session 
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['POST'])
+def login():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    
+    if username and password:
+        user = User.query.filter_by(username=username).first()
+        print("user:", username)
+        print("user.username:", user.username)
+        print("user.password:", user.password)
+        print("password:", password)
+        if user and user.password == password:
+            login_user(user)
+            return jsonify({"message": "Login successful"}), 200
+
+    return jsonify({"message": "Invalid username or password"}), 400
+
+@app.route('/logout', methods=['GET'])
+@login_required
+def logout():
+    logout_user()
+    return jsonify({"message": "Logout successful"}), 200
+
+@app.route('/user', methods=['POST'])
+@login_required
+def create_user():
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+
+    if username and password:
+        user = User(username=username, password=password)
+        db.session.add(user)
+        db.session.commit()
+        return jsonify({"message": "User created successfully"}), 201
+
+    return jsonify({"message": "Invalid username or password"}), 400
+
+@app.route('/user/<int:user_id>', methods=['GET'])
+@login_required
+def read_user(user_id):
+    user = User.query.get(user_id)
+    if user:
+        return jsonify({"user": user.to_dict()}), 200
+    else:
+        return jsonify({"message": "User not found"}), 404
+
+@app.route('/user/<int:user_id>', methods=['PUT'])
+@login_required
+def update_user(user_id):
+    data = request.get_json()
+    username = data.get('username')
+    password = data.get('password')
+    
+    if username and password:
+        user = User.query.get(user_id)
+        if user:
+            user.username = username
+            user.password = password
+            db.session.commit()
+            return jsonify({"message": "User updated successfully"}), 200
+        else:
+            return jsonify({"message": "User not found"}), 404
+    
+    return jsonify({"message": "Invalid username or password"}), 400
+
+@app.route('/user/<int:user_id>', methods=['DELETE'])
+@login_required
+def delete_user(user_id):
+    user = User.query.get(user_id)
+
+    if user and user.id == user_id:
+        return jsonify({"message": "User not authorized to delete this user"}), 403
+        
+    if user :
+        db.session.delete(user)
+        db.session.commit()
+        return jsonify({"message": "User deleted successfully"}), 200
+    else:
+        return jsonify({"message": "User not found"}), 404
+
+@app.route('/users', methods=['GET'])
+@login_required
+def get_users():
+    users = User.query.all()
+    return jsonify({"users": [user.to_dict() for user in users]}), 200
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ models/user.py
@@ -0,0 +1,11 @@
+from database import db
+from flask_login import UserMixin
+
+# modelo de usuario
+class User(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
+
+    def to_dict(self):
+        return {"id": self.id, "username": self.username}
